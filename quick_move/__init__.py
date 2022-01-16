26bead5d35997fb96af7abc572c84baa162f071a
Tyler Johnson <tjohn73@uwo.ca>
2022-01-15 20:32:21 -0500

    Now this can move indiv. files

--- quick_move/__init__.py
+++ quick_move/__init__.py
@@ -1,13 +1,14 @@
 import os
 import sys
 import asyncio
+import threading
+from math import pow
+
+# Local
 from . import DirList
 from . import cli
 from . import Hash
-import threading
 from . import Transfer
-import ctypes
-from math import pow
 
 def Calcualate_Size(bytes: int) -> str:
 
@@ -45,21 +46,6 @@ async def program():
     source = argv[1]
     dest   = argv[2]
 
-    # Create the lists (relative paths)
-    print('Searching ' + source + '...')
-    src_dir_list  = DirList.CreateDirMap(source) 
-    
-    print('Searching ' + dest + '...')
-    dest_dir_list = DirList.CreateDirMap(dest)
-
-    # Remove the beginning directory
-    src_dir_list = [dir[len(source) + 1:] for dir in src_dir_list]
-    dest_dir_list = [dir[len(dest) + 1:] for dir in dest_dir_list]
-
-    # Quickly create the main directory if it does not exist
-    if not os.path.exists(dest):
-        os.mkdir(dest)
-
     # Stuff here is used for UI
     # Number of bytes
     bytes_written = [0]
@@ -68,45 +54,76 @@ async def program():
     # These are the source directories to write and where to write as tuples (src, dest)
     files_to_write = list()
 
-    print("Finding modified files...")
-    # Iterate through each path and detect files that need to be replaced (Also create the nessary folders)
-    for src_path in src_dir_list:
+    if os.path.isfile(source):
 
-        # Paths with directory at the front
-        abs_src_path  = '/'.join([source, src_path])
-        abs_dest_path = '/'.join([dest, src_path]) 
-
-        # If both paths exists (This is why the top directory is removed from the list)
-        if src_path in dest_dir_list:
-
-            # If this is a file
-            if os.path.isfile(abs_src_path):
-                
-                h1 = Hash.hashFile(abs_src_path)
-                h2 = Hash.hashFile(abs_dest_path)
-
-                # If files is different
-                if h1 != h2:
-                    files_to_write.append((abs_src_path, abs_dest_path))
-
-            # If it is an already existing file
-            else:
-                pass
-
-        # If path does not exist
+        # File doesn't exist, so write
+        if not os.path.exists(dest):
+            files_to_write.append((source, dest))
+        
         else:
-
-            # Create directory or file or whatever the thing is
-            if os.path.isdir(abs_src_path):
-                os.mkdir(abs_dest_path)
             
-            # else if this is a file that doesn't exist, transfer it
-            else:
-                
-                files_to_write.append((abs_src_path, abs_dest_path))
-                
+            h1 = Hash.hashFile(source)
+            h2 = Hash.hashFile(dest)
 
-    # End of detection for loop
+            if h1 != h2:
+                files_to_write.append((source, dest))
+
+    elif os.path.isdir(source) and os.path.isdir(dest):
+        
+        # Create the lists (relative paths)
+        print('Searching ' + source + '...')
+        src_dir_list  = DirList.CreateDirMap(source) 
+        
+        print('Searching ' + dest + '...')
+        dest_dir_list = DirList.CreateDirMap(dest)
+
+        # Remove the beginning directory
+        src_dir_list = [dir[len(source) + 1:] for dir in src_dir_list]
+        dest_dir_list = [dir[len(dest) + 1:] for dir in dest_dir_list]
+
+        # Quickly create the main directory if it does not exist
+        if not os.path.exists(dest):
+            os.mkdir(dest)
+
+        print("Finding modified files...")
+        # Iterate through each path and detect files that need to be replaced (Also create the nessary folders)
+        for src_path in src_dir_list:
+
+            # Paths with directory at the front
+            abs_src_path  = '/'.join([source, src_path])
+            abs_dest_path = '/'.join([dest, src_path]) 
+
+            # If both paths exists (This is why the top directory is removed from the list)
+            if src_path in dest_dir_list:
+
+                # If this is a file
+                if os.path.isfile(abs_src_path):
+                    
+                    h1 = Hash.hashFile(abs_src_path)
+                    h2 = Hash.hashFile(abs_dest_path)
+
+                    # If files is different
+                    if h1 != h2:
+                        files_to_write.append((abs_src_path, abs_dest_path))
+
+                # If it is an already existing file
+                else:
+                    pass
+
+            # If path does not exist
+            else:
+
+                # Create directory or file or whatever the thing is
+                if os.path.isdir(abs_src_path):
+                    os.mkdir(abs_dest_path)
+                
+                # else if this is a file that doesn't exist, transfer it
+                else:
+                    
+                    files_to_write.append((abs_src_path, abs_dest_path))
+                    
+
+    # Start transfering files
     print("Calculating size to write...")
     total_bytes = sum([os.path.getsize(item[0]) for item in files_to_write])
 
